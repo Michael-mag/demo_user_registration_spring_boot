4d486a532bd6e607c8318245e75b6b97943dad15
Michael-Mag <michaelpmagaisa@gmail.com>
2022-02-24 23:52:14 +0100

    send confirmation email to user.

---
+++ src/main/java/com/michael/magaisa/demo_user_registration_app/email/EmailSender.java
@@ -0,0 +1,5 @@
+package com.michael.magaisa.demo_user_registration_app.email;
+
+public interface EmailSender {
+    void send(String reciepient, String emailMsg);
+}
---
+++ src/main/java/com/michael/magaisa/demo_user_registration_app/email/EmailService.java
@@ -0,0 +1,37 @@
+package com.michael.magaisa.demo_user_registration_app.email;
+
+
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+@AllArgsConstructor
+public class EmailService implements EmailSender{
+    private final static Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
+    private final JavaMailSender mailSender;
+
+    @Override
+    @Async
+    public void send(String reciepient, String emailMsg) {
+        try {
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+            helper.setText(emailMsg, true);
+            helper.setTo(reciepient);
+            helper.setSubject("Confirm your email.");
+            helper.setFrom("hello@michael-mag.com");
+            mailSender.send(mimeMessage);
+        }catch (MessagingException e){
+            LOGGER.error("Failed to send the email", e);
+            throw new IllegalStateException("Failed to send email.");
+        }
+    }
+}
